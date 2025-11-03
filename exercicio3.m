f2b38ea613721e286f631138bcea70018756a467
grethaflorian <floriangretha@gmail.com>
2025-11-03 15:23:54 -0300

    Update exercicio3.m

--- exercicio3.m
+++ exercicio3.m
@@ -1,31 +1,34 @@
 function [mu] = exercicio3(t, NC)
+% exercicio3 - Calcula a taxa de crescimento (mu) de células
+%
+% Entradas:
+%   t  - vetor de tempo com instâncias das medições
+%   NC - vetor com o número de células em cada instante t
+%
+% Saída:
+%   mu - taxa de crescimento estimada
 
-% t: vetor de tempo com instâncias das medições
-% NC: medida do número de células a cada instante t
+% ==============================
+tempo_quad = t.^2;
+N = length(t);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-tempo_quad = t.^2
+soma_tempo = sum(t);
+soma_tempo_quad = sum(tempo_quad);
 
-N = length(t)
+ln_celulas = log(NC);
+soma_ln_celulas = sum(ln_celulas);
 
-soma_tempo = sum(t)
-soma_tempo_quad = sum(tempo_quad) 
+tempo_X_ln_celulas = t .* ln_celulas;
+soma_do_produto = sum(tempo_X_ln_celulas);
 
-ln_celulas = log(NC)
+a1 = (N * soma_do_produto - soma_tempo * soma_ln_celulas) / ...
+     (N * soma_tempo_quad - soma_tempo^2);
 
-soma_ln_celulas = sum(ln_celulas) 
+a0 = soma_ln_celulas / N - a1 * (soma_tempo / N);
 
-tempo_X_ln_celulas = t.*ln_celulas
-
-soma_do_produto = sum(tempo_X_ln_celulas)
-
-a1 = (N*soma_do_produto - soma_tempo*soma_ln_celulas)/(N*soma_tempo_quad - soma_tempo^2)
-
-a0 = soma_ln_celulas/N - a1*(soma_tempo/N)
-
-% mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
+% ==============================
+% Valor final solicitado
 mu = a1;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
 
-endfunction
